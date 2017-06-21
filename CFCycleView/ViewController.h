3ef3ad78fea72adcc2442a7b6c5c48d0db91152b
caofeng <cao.feng@zyxr.com>
2017-06-21 15:07:04 +0800

    修改

--- CFCycleView/ViewController.h
+++ CFCycleView/ViewController.h
@@ -3,7 +3,7 @@
 //  CFCycleView
 //
 //  Created by CaoFeng on 2017/6/21.
-//  Copyright © 2017年 深圳中业兴融互联网金融服务有限公司. All rights reserved.
+//  Copyright © 2017年 CaoFeng. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
